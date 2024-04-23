28d884d5a66c2b188f499e0398da58e6fd69b3ad
R-G2110 <argeldelacruz86@gmail.com>
2024-04-23 12:50:05 +0200

    [Add]: Sum function

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace csharp_calcolatrice
+{
+    internal class CalcoliHelper
+    {
+        // Metodo per la somma di due numeri interi
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
